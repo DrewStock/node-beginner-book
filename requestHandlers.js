26ae21017aea6c5daadd8283482e15a7a7bdcfa8
DrewStock <andrewdstock@gmail.com>
2016-11-26 19:34:58 -0800

    completed reading/tutorial

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,29 +1,58 @@
-const exec = require('child_process').exec;
+const querystring = require('querystring');
+const fs = require('fs');
+const formidable = require('formidable');
 
 function start(response) {
   console.log('Request handler -start was called');
 
-  // exec('find /', { timeout: 10000, maxBuffer: 20000*1024 },
-  // function (error, stdout, stderr) {
-  //   response.writeHead(200, {'Content-Type': 'text/plain'});
-  //   response.write(stdout);
-  //   response.end();
-  // });
+  var body =
+  '<!DOCTYPE html>' +
+  '<html>' +
+  '<head>' +
+  '<meta http-equiv="Content-Type" content="text/html; ' +
+  'charset=UTF-8" />' +
+  '</head>' +
+  '<body>' +
+  '<form action="/upload" enctype="multipart/form-data" ' + 'method="post">' +
+  '<input type="file" name="upload">' +
+  '<input type="submit" value="Upload file">' +
+  '</form>' +
+  '</body>' +
+  '</html>';
 
-  exec('ls -lah', function (error, stdout, stderr) {
-    response.writeHead(200, {'Content-type':'text/plain'});
-    response.write(stdout);
+  response.writeHead(200, {'Content-type':'text/html'});
+  response.write(body);
+  response.end();
+}
+
+function upload(response, request) {
+  console.log('Request handler -upload was called');
+  var form = new formidable.IncomingForm();
+  console.log('about to parse');
+  form.parse(request, function(error, fields, files) {
+    console.log('parsing done');
+    fs.rename(files.upload.path, './tmp/test.png', function(error) {
+      if (error) {
+        fs.unlink('./tmp/test.png');
+        fs.rename(files.upload.path, './tmp/test.png');
+      }
+    });
+
+    response.writeHead(200, {'Content-type':'text/html'});
+    response.write('received image:<br/>');
+    response.write("<img src='/show' />");
     response.end();
   });
 }
 
-function upload(response) {
-  console.log('Request handler -upload was called');
-  response.writeHead(200, {'Content-type':'text/plain'});
-  response.write('Hello Upload');
-  response.end();
+function show(response) {
+  console.log('Request handler -show was called');
+  response.writeHead(200, {'Content-Type':'image/png'});
+  fs.createReadStream('./tmp/test.png').pipe(response);
 }
 
 exports.start = start;
 
 exports.upload = upload;
+
+exports.show = show;
